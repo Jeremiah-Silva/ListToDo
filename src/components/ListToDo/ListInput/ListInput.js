81f0c5acf37b304585fb3ba95248e3d5264933d2
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-07-18 13:11:55 -0600

    Working with CSS style.

--- src/components/ListToDo/ListInput/ListInput.js
+++ src/components/ListToDo/ListInput/ListInput.js
@@ -5,21 +5,30 @@ import './ListInput.css';
 
 const ListInput = props => {
   const [enteredTask, setEnteredTask] = useState('');
+  const [isValid, setIsValid] = useState();
 
   const listInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredTask(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredTask.trim().length === 0) {
+      setIsValid(false);
+      return;  
+    }
     props.onAddTask(enteredTask);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>List to Do:</label>
-        <input type="text" onChange={listInputChangeHandler} />
+        <input type="text" onChange={listInputChangeHandler} 
+              />
       </div>
       <Button type="submit">Add Task</Button>
     </form>
