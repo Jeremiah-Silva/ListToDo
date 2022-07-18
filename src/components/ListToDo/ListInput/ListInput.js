cb89c1c83ad9d187c77561d8e668602c9c4da796
Jeremiah Silva <jeremiah.ca9@gmail.com>
2022-07-18 13:53:30 -0600

    Built some style CSS.

--- src/components/ListToDo/ListInput/ListInput.js
+++ src/components/ListToDo/ListInput/ListInput.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './ListInput.css';
+
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${props => (props.invalid ? 'red' : 'black')};
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid ${props => (props.invalid ? 'red' : 'rgb(168, 184, 225)')};
+  background: ${props => props.invalid ? '#ffd7d7' : 'transparent' };
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: rgb(168, 184, 225);
+  border-color: #000080;
+}
+`;
 
 const ListInput = props => {
   const [enteredTask, setEnteredTask] = useState('');
@@ -25,11 +54,11 @@ const ListInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}> 
         <label>List to Do:</label>
-        <input type="text" onChange={listInputChangeHandler} 
-              />
-      </div>
+        <input type="text" 
+              onChange={listInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Task</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
 
+const Button = styled.button`
+  with: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #000080;
+  color: white;
+  background: #000080;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  @media (min-width: 786px) {
+    width: auto;
+  }
+
+&:focus {
+  outline: none;
+}
+
+&button:hover,
+&button:active {
+  background: blue;
+  border-color: blue;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+`;
+export default Button;
+
+/*
 const Button = props => {
   return (
     <button type={props.type} 
@@ -12,5 +39,4 @@ const Button = props => {
     </button>
   );
 };
-
-export default Button;
+*/
